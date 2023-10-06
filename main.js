510e893cb49de807b4094e6986d2afc06dc624a2
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-10-06 11:29:49 -0300

    aaaaaaaaaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function tocaSomMsg(){
     document.querySelector('#som_tecla_olha_msg').play();
 }
+function tocaSomOwwn(){
+    document.querySelector('som_tecla_owwn').play()
+}
+function tocaSomZZZ(){
+    document.querySelector('som_tecla_zzzz').play()
+}
+function tocaSomDia(){
+    document.querySelector('som_tecla_bom_dia').play()
+}
