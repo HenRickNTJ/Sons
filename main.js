5a9a6f7e3bea64e42c09ace365f973b2ebd5f6d8
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-10-20 10:59:19 -0300

    a

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ document.querySelector('.tecla_olha_msg').onclick = tocaSomMsg;
 function tocaSomGmd (){
     document.querySelector('#som_tecla_owwn').play();
 }
-//chamada ou invocação da função tocaSomMsg ao clicar no botão
-document.querySelector('.tecla tecla_owwn').onclick = tocaSomGmd;
+//chamada ou invocação da função tocaSomGmd ao clicar no botão
+document.querySelector('.tecla_owwn').onclick = tocaSomGmd;
