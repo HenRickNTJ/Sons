50dfad11e2a1e379c511a5c7f49b99cd602ffa56
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-10-20 11:02:59 -0300

    aaaaaaaaaa

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ function tocaSomGmd (){
 }
 //chamada ou invocação da função tocaSomGmd ao clicar no botão
 document.querySelector('.tecla_owwn').onclick = tocaSomGmd;
+
+//Declaração/criação da função tocaSomZzz
+function tocaSomZzz (){
+    document.querySelector('#som_tecla_zzzz').play();
+}
+//chamada ou invocação da função tocaSomZzz ao clicar no botão
+document.querySelector('.tecla_zzzz').onclick = tocaSomZzz;
+
+//Declaração/criação da função tocaSomDia
+function tocaSomDia (){
+    document.querySelector('#som_tecla_bom_dia').play();
+}
+//chamada ou invocação da função tocaSomDia ao clicar no botão
+document.querySelector('.tecla_bom_dia').onclick = tocaSomDia;
