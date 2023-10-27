9e5b4a8d20b9baabbd5179f75c947fcb9707ec25
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-10-27 10:27:21 -0300

    aaaa

--- main.js
+++ main.js
@@ -2,66 +2,13 @@
 function tocaSomMsg (){
     document.querySelector('#som_tecla_olha_msg').play();
 }
-//chamada ou invocação da função tocaSomMsg ao clicar no botão
-document.querySelector('.tecla_olha_msg').onclick = tocaSomMsg;
-
-//Declaração/criação da função tocaSomGmd
-function tocaSomGmd (){
-    document.querySelector('#som_tecla_owwn').play();
-}
-//chamada ou invocação da função tocaSomGmd ao clicar no botão
-document.querySelector('.tecla_owwn').onclick = tocaSomGmd;
-
-//Declaração/criação da função tocaSomZzz
-function tocaSomZzz (){
-    document.querySelector('#som_tecla_zzzz').play();
-}
-//chamada ou invocação da função tocaSomZzz ao clicar no botão
-document.querySelector('.tecla_zzzz').onclick = tocaSomZzz;
-
-//Declaração/criação da função tocaSomDia
-function tocaSomDia (){
-    document.querySelector('#som_tecla_bom_dia').play();
-}
-//chamada ou invocação da função tocaSomDia ao clicar no botão
-document.querySelector('.tecla_bom_dia').onclick = tocaSomDia;
-
-//Declaração/criação da função tocaSomGosta
-function tocaSomGosta (){
-    document.querySelector('#som_tecla_ele_gosta').play();
-}
-//chamada ou invocação da função tocaSomGosta ao clicar no botão
-document.querySelector('.tecla_ele_gosta').onclick = tocaSomGosta;
-
-//Declaração/criação da função tocaSomIhaa
-function tocaSomIhaa (){
-    document.querySelector('#som_tecla_IHÁÁÁ').play();
-}
-//chamada ou invocação da função tocaSomIhaa ao clicar no botão
-document.querySelector('.tecla_IHÁÁÁ').onclick = tocaSomIhaa;
-
-//Declaração/criação da função tocaSomBgd
-function tocaSomBgd (){
-    document.querySelector('#som_tecla_obrigado').play();
-}
-//chamada ou invocação da função tocaSomBgd ao clicar no botão
-document.querySelector('.tecla_obrigado').onclick = tocaSomBgd;
-
-//Declaração/criação da função tocaGosto
-function tocaSomGosto (){
-    document.querySelector('#som_tecla_gosto').play();
-}
-//chamada ou invocação da função tocaSomGosto ao clicar no botãotecla_olha_msg
-document.querySelector('.tecla_gosto').onclick = tocaSomGosto;
-
-//Declaração/criação da função tocaSomUepa
-function tocaSomUepa (){
-    document.querySelector('#som_tecla_ueepa').play();
-}
-//chamada ou invocação da função tocaSomUepa ao clicar no botão
-document.querySelector('.tecla_ueepa').onclick = tocaSomUepa;
 
 //criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('tecla');
-//chamando a constante listaDeTeclas e o índice 0 do somMsg
-listaDeTeclas[0].onclick = tocaSomMsg
+
+let contador = 0;
+while(contador < 9){
+    listaDeTeclas[contador].onclick = tocaSomMsg;
+    contador = contador + 1;
+    console.log(contador);
+}
