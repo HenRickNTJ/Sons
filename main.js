f413bd819ca9a32321e029bd0ee464cf881f7b97
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-10-27 10:56:55 -0300

    aa

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som" + efeito;
+    const idAudio = "#som_" + efeito;
     listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio)
     }
