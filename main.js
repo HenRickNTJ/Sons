ef81c365c5f75f0513839cd852c697010a4cb2fa
Henrique Vieira <105082139+HenRickNTJ@users.noreply.github.com>
2023-11-17 10:27:41 -0300

    aaaaaaaaa

--- main.js
+++ main.js
@@ -3,17 +3,12 @@ function tocaSom (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-//criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//criação da repetição "while" e trocado "for" com variável, condição e incremento 
 for(let contador = 0; contador < listaDeTeclas.length; contador = contador++){
-    const teclas = listaDeTeclas[contador] //guarda o contador da lista 
-    const efeito = teclas.classList[1];  //chama o 2
-    const idAudio = `#som_${efeito}`;    const teclas = listaDeTeclas[contador] //guarda o contador da lista 
-    const efeito = teclas.classList[1];  //chama o 2° item classe individual do som 
-    const idAudio = `#som_${efeito}`;  //utiliza a junção template string ${}
-  teclas.onclick = function(){  //função anônima auxiliar para chamar tocaSom
-        tocaSom(idAudio); //chamada da função tocaSom com parâmetro idAudio 
-    }
+    const teclas = listaDeTeclas[contador] 
+    const efeito = teclas.classList[1];
+    const idAudio = `#som_${efeito}`;  
+        tocaSom(idAudio); 
+}
 }
